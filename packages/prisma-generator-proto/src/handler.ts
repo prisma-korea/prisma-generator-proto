f59a402c2110899d17ccdea2a2b9eb633020dbdd
Donghyeon Kim <0916dhkim@gmail.com>
2022-01-16 16:01:57 -0500

    Add schema and mappings handlers

--- packages/prisma-generator-proto/src/handler.ts
+++ packages/prisma-generator-proto/src/handler.ts
@@ -2,12 +2,23 @@ import { DMMF } from "@prisma/generator-helper";
 
 export function generateHandler(dmmf: DMMF.Document): string {
   const datamodelJson = JSON.stringify(dmmf.datamodel);
+  const schemaJson = JSON.stringify(dmmf.schema);
+  const mappingsJson = JSON.stringify(dmmf.mappings);
+
   const file = `import { Router } from "express";
 export const router = Router();
 
-router.get("/datamodels", (req, res) => {
+router.get("/datamodel", (req, res) => {
   res.send(${datamodelJson});
-})
+});
+
+router.get("/schema", (req, res) => {
+  res.send(${schemaJson});
+});
+
+router.get("/mappings", (req, res) => {
+  res.send(${mappingsJson});
+});
 `;
   return file;
 }
