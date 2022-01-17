4f393e904ae26d1b120b6e6efec09a616194f5ba
Taehyeon Kim <1234tgk@gmail.com>
2022-01-16 22:27:36 -0500

    add tables and forms

--- example/web/src/screens/Continents.tsx
+++ example/web/src/screens/Continents.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import styles from "../styles/Table.module.css";
 
 type Continent = {
   id: string;
@@ -7,35 +8,70 @@ type Continent = {
 
 const Continents = () => {
   const [continents, setContinents] = useState<Continent[]>([]);
+  const [name, setName] = useState<string>("");
 
   useEffect(() => {
-    fetch("/continent", {
+    fetch("/api/continent", {
       method: "GET",
     })
       .then((res) => res.json())
       .then((json) => setContinents(json));
   }, [setContinents]);
 
+  const addHandler: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    const body = {
+      name,
+    };
+
+    fetch("/api/continent", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((json: Continent) => {
+        setContinents([...continents, json]);
+        setName("");
+      });
+  };
+
   return (
-    <div>
-      <h1>Continents</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Continent ID</th>
-            <th>Continent Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {continents.map((continent) => (
-            <tr key={continent.id}>
-              <td>{continent.id}</td>
-              <td>{continent.name}</td>
+    <div className={styles.container}>
+      <div>
+        <h1>Continents</h1>
+
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Continent ID</th>
+              <th>Continent Name</th>
             </tr>
-          ))}
-        </tbody>
-        <tfoot></tfoot>
-      </table>
+          </thead>
+          <tbody>
+            {continents.map((continent) => (
+              <tr key={continent.id}>
+                <td>{continent.id}</td>
+                <td>{continent.name}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot></tfoot>
+        </table>
+
+        <form className={styles.form} onSubmit={addHandler}>
+          <h1>Add Continent</h1>
+          <input
+            placeholder='continent name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input type='submit' value='Add' />
+        </form>
+      </div>
     </div>
   );
 };
--- example/web/src/screens/Countries.tsx
+++ example/web/src/screens/Countries.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import styles from "../styles/Table.module.css";
 
 type Country = {
   id: string;
@@ -9,36 +10,80 @@ type Country = {
 const Countries = () => {
   const [countries, setCountries] = useState<Country[]>([]);
 
+  const [name, setName] = useState<string>("");
+  const [continentId, setContinentId] = useState<string>("");
+
   useEffect(() => {
-    fetch("/country", {
+    fetch("/api/country", {
       method: "GET",
     })
       .then((res) => res.json())
       .then((json) => setCountries(json));
   }, [setCountries]);
 
+  const addHandler: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    const body = {
+      name,
+      continentId,
+    };
+
+    fetch("/api/country", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((json: Country) => {
+        setCountries([...countries, json]);
+        setName("");
+        setContinentId("");
+      });
+  };
+
   return (
-    <div>
-      <h1>Countries</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Country ID</th>
-            <th>Country Name</th>
-            <th>Continent ID</th>
-          </tr>
-        </thead>
-        <tbody>
-          {countries.map((country) => (
-            <tr key={country.id}>
-              <td>{country.id}</td>
-              <td>{country.name}</td>
-              <td>{country.continentId}</td>
+    <div className={styles.container}>
+      <div>
+        <h1>Countries</h1>
+
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Country ID</th>
+              <th>Country Name</th>
+              <th>Continent ID</th>
             </tr>
-          ))}
-        </tbody>
-        <tfoot></tfoot>
-      </table>
+          </thead>
+          <tbody>
+            {countries.map((country) => (
+              <tr key={country.id}>
+                <td>{country.id}</td>
+                <td>{country.name}</td>
+                <td>{country.continentId}</td>
+              </tr>
+            ))}
+          </tbody>
+          <tfoot></tfoot>
+        </table>
+
+        <form className={styles.form} onSubmit={addHandler}>
+          <h1>Add Country</h1>
+          <input
+            placeholder='country name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            placeholder='continent id'
+            value={continentId}
+            onChange={(e) => setContinentId(e.target.value)}
+          />
+          <input type='submit' value='Add' />
+        </form>
+      </div>
     </div>
   );
 };
