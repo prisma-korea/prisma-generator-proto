457435b85882ea7f8b018f0f61064ee6e2e1bfc6
Donghyeon Kim <0916dhkim@gmail.com>
2022-02-05 11:01:13 -0500

    Improve hook types

--- packages/prisma-generator-proto/src/generate/hooks.ts
+++ packages/prisma-generator-proto/src/generate/hooks.ts
@@ -12,13 +12,6 @@ const FIELD_TO_TS = new Map<
 ]);
 
 /**
- * Find the name of id field from the model. `undefined` if no id.
- */
-function findIdFieldName(model: DMMF.Model): string | undefined {
-  return model.fields.find((field) => field.isId)?.name;
-}
-
-/**
  * Convert Prisma field type to the matching TS type.
  */
 function getFieldTsType(field: DMMF.Field): string {
@@ -29,9 +22,31 @@ function getFieldTsType(field: DMMF.Field): string {
 }
 
 /**
+ * Calculate the TS type of the model's ID.
+ */
+function getIdTsType(model: DMMF.Model): string {
+  for (const field of model.fields) {
+    if (field.isId) return getFieldTsType(field);
+  }
+  return "never";
+}
+
+/**
+ * @returns `true` if creating a row will fail without specifying the given field.
+ */
+function needsInitialValue(field: DMMF.Field): boolean {
+  return (
+    field.kind === "scalar" && (!field.isRequired || !field.hasDefaultValue)
+  );
+}
+
+/**
  * Find appropriate initial value for the field.
  */
 function getFieldInitialValue(field: DMMF.Field): unknown {
+  if (!needsInitialValue(field)) {
+    return undefined;
+  }
   const fieldMetadata = FIELD_TO_TS.get(field.type);
   if (fieldMetadata === undefined)
     throw new Error(`Unsupported field type ${field.type}`);
@@ -39,111 +54,117 @@ function getFieldInitialValue(field: DMMF.Field): unknown {
 }
 
 function generateFieldType(field: DMMF.Field): string {
-  return `${field.name}: ${getFieldTsType(field)} ${
-    field.isRequired ? "" : "|null|undefined"
-  };`;
+  let fieldType = field.name;
+  fieldType += !field.isRequired ? "?: " : ": ";
+  fieldType += getFieldTsType(field);
+  if (!field.isRequired) {
+    fieldType += " | undefined | null";
+  }
+  return fieldType;
 }
 
 /**
- * Generate filtered fields
+ * Make a list of scalar fields from the model.
  */
-function filterFields(model: DMMF.Model): DMMF.Field[] {
+function filterScalarFields(model: DMMF.Model): DMMF.Field[] {
   return model.fields.filter((field) => field.kind === "scalar");
+}
+
+/**
+ * Generate part of FormState type.
+ */
+function generateFormStateElement(field: DMMF.Field): string {
+  const fieldTsType = getFieldTsType(field);
+  const typeType = `type: "${fieldTsType}"`;
+  const valueType = needsInitialValue(field)
+    ? `value: ${fieldTsType}`
+    : `value?: ${fieldTsType} | undefined`;
+  return `${field.name}: {${typeType}; ${valueType}};`;
 }
 
 /**
  * Generate TS code for model type declaration.
  *
  * Client code do not handle nested values, so leave them out.
- * Omit id field from FormValues type if it exists.
  */
 function generateClientType(model: DMMF.Model): string {
-  const editableFields = filterFields(model);
-  const idFieldName = findIdFieldName(model);
-  const formValueType = idFieldName
-    ? `Omit<${model.name}, "${idFieldName}">`
-    : model.name;
+  const scalarFields = filterScalarFields(model);
   return `
 export type ${model.name} = {
-  ${editableFields.map(generateFieldType).join("\n  ")}
+  ${scalarFields.map(generateFieldType).join("\n  ")}
 };
-export type ${model.name}FormValues = ${formValueType};`;
-}
-
-/**
- * Generate array of objects where each object has
- * the name of the key, and its type.
- *
- * Primary key is omitted
- */
-function generateFieldsMetaData(model: DMMF.Model): string {
-  const idFieldName = findIdFieldName(model);
-  const editableFields = filterFields(model)
-    .filter((field) => field.name !== idFieldName)
-    .map((field) => {
-      return {
-        name: field.name,
-        type: getFieldTsType(field),
-      };
-    });
-
-  return `const ${model.name}FieldsMetaData = ${JSON.stringify(
-    editableFields
-  )};`;
+export type ${model.name}List = {
+  id: ${getIdTsType(model)};
+  fields: ${model.name};
+}[];
+export type ${model.name}FormState = {
+  ${scalarFields.map(generateFormStateElement).join("\n  ")}
+};`;
 }
 
 function generateSingleHook(model: DMMF.Model): string {
   const initialValue: Record<string, unknown> = {};
   for (const field of model.fields) {
-    if (!field.isId && field.kind === "scalar") {
+    if (field.kind === "scalar") {
       initialValue[field.name] = getFieldInitialValue(field);
+      initialValue[field.name] = {
+        type: getFieldTsType(field),
+        value: getFieldInitialValue(field),
+      };
     }
   }
 
   return `
 ${generateClientType(model)}
-export const use${model.name} = (): [${
-    model.name
-  }[], React.FormEventHandler<HTMLFormElement>,
-  ${model.name}FormValues, 
-  React.Dispatch<React.SetStateAction<${model.name}FormValues>>,
-  FormFields] => {
-  const initialValue = ${JSON.stringify(initialValue)}
+export const use${model.name} = (): [
+  ${model.name}List,
+  React.FormEventHandler<HTMLFormElement>,
+  ${model.name}FormState, 
+  React.Dispatch<React.SetStateAction<${model.name}FormState>>,
+] => {
+  const INITIAL_VALUE: ${model.name}FormState = ${JSON.stringify(initialValue)};
 
-  const [state, setState] = useState<${model.name}[]>([]);
-  const [values, setValues] = useState<${model.name}FormValues>(initialValue);
+  const [list, setList] = useState<${model.name}List>([]);
+  const [formState, setFormState] = useState<${
+    model.name
+  }FormState>(INITIAL_VALUE);
 
   useEffect(() => {
     fetch("/api/${model.name.toLowerCase()}", {
       method: "GET"
     })
     .then(res => res.json())
-    .then((json: ${model.name}[]) => setState(json));
-  }, [setState]);
+    .then((json: ${
+      model.name
+    }[]) => setList(json.map(e => ({id: e.id, fields: e}))));
+  }, []);
 
   const addHandler: React.FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
 
+    // Calculate request payload from form state.
+    const payload: Record<string, unknown> = {};
+    for (const [key, something] of Object.entries(formState)) {
+      payload[key] = something.value;
+    }
     fetch("/api/${model.name.toLowerCase()}", {
       method: "POST",
       headers: {
         "content-type": "application/json",
       },
-      body: JSON.stringify(values),
+      body: JSON.stringify(payload),
     })
     .then((res) => res.json())
       .then((json: ${model.name}) => {
-        setState([...state, json]);
+        setList([...list, {id: json.id, fields: json}]);
       })
       .catch(() => alert("${model.name} could not be created"))
       .finally(() => {
-        setValues(initialValue);
+        setFormState(INITIAL_VALUE);
       });
   };
 
-  ${generateFieldsMetaData(model)}
-
-  return [state, addHandler, values, setValues, ${model.name}FieldsMetaData];
+  return [list, addHandler, formState, setFormState];
 };
 `;
 }
