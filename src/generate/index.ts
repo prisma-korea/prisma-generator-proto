d80dcbe7f59eae5a699d482c1af831189619e6e2
Donghyeon Kim <0916dhkim@gmail.com>
2022-10-22 14:59:26 -0400

    Link CMS to the parent project

--- src/generate/index.ts
+++ src/generate/index.ts
@@ -2,6 +2,7 @@ import * as fs from "fs-extra";
 import * as path from "path";
 
 import { DMMF } from "@prisma/generator-helper";
+import { exec } from "child_process";
 import { generate } from "./eta";
 
 async function writeFileAnywhere(
@@ -10,6 +11,22 @@ async function writeFileAnywhere(
 ): Promise<void> {
   await fs.ensureDir(path.dirname(filepath));
   return fs.writeFile(filepath, data);
+}
+
+/** Find the absolute path to the parent project. */
+function findParentPath(): Promise<string> {
+  return new Promise((resolve, reject) => {
+    // Assuming Prisma generator is run inside the parent project,
+    // `npm prefix` command will return the absolute path to
+    // the directory containing parent project's `package.json` file.
+    exec("npm prefix", (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result.trim());
+      }
+    });
+  });
 }
 
 /**
@@ -23,11 +40,19 @@ export async function generateAndEmit(
   outdir: string,
   prismaClientPath: string
 ): Promise<void> {
+  const absoluteParentPath = await findParentPath();
+
   await fs.copy(path.join(__dirname, "../../template"), outdir, {
     overwrite: true,
     recursive: true,
   });
 
+  await writeFileAnywhere(
+    path.join(outdir, "package.json"),
+    await generate("/views/package.json.eta", {
+      parentLink: path.relative(outdir, absoluteParentPath),
+    })
+  );
   await writeFileAnywhere(
     path.join(outdir, "tsconfig.json"),
     await generate("/views/tsconfig.json.eta", {
@@ -35,7 +60,23 @@ export async function generateAndEmit(
     })
   );
   await writeFileAnywhere(
+    path.join(outdir, "app/prisma.tsx"),
+    await generate("/views/prisma.tsx.eta", {
+      prismaClientModule: path.join(
+        "parent",
+        path.relative(absoluteParentPath, prismaClientPath)
+      ),
+    })
+  );
+  await writeFileAnywhere(
     path.join(outdir, "app/routes/index.tsx"),
     await generate("/views/home.tsx.eta", { models: dmmf.datamodel.models })
   );
+
+  for (const model of dmmf.datamodel.models) {
+    await writeFileAnywhere(
+      path.join(outdir, `app/routes/${model.name}.tsx`),
+      await generate("/views/model.tsx.eta", { model })
+    );
+  }
 }
